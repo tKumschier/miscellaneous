2c843ad194b0fe9e2d921e5067d1d13e5f218cfc
Angelicornis <dev.angelicornis@icloud.com>
2023-12-24 15:07:15 +0100

    Adapt logger to reset save_path

--- miscellaneous/logger.py
+++ miscellaneous/logger.py
@@ -19,28 +19,25 @@ class Logger(BaseModel):
     logger: logging.Logger = Field(...)
     log_level_is_on: dict[str, bool] = Field(...)
     problem_occurred: bool = False
-    save_path: Path = Field(...)
+    log_level: str = "INFO"
 
     class Config:
         arbitrary_types_allowed = True
 
-    def __init__(self, log_level: str = "INFO") -> None:
-        save_path: Path = (
-            Path(__file__).absolute().parents[2]
-            / "logs"
-            / (datetime.now().strftime("%Y.%m.%d_%H.00") + ".log")
-        )
-        logger = self._get_logger(log_level, save_path)
+    def __init__(self) -> None:
+        logger = self._get_logger()
         log_level_is_on: dict[str, bool] = {
             key: logger.isEnabledFor(val) for (key, val) in LOG_LEVELS.items()
         }
-        super().__init__(
-            logger=logger, log_level_is_on=log_level_is_on, save_path=save_path
-        )
+        super().__init__(logger=logger, log_level_is_on=log_level_is_on)
 
-    def _get_logger(
-        self, log_level: str, save_path: Path, logger_name: str = "root"
-    ) -> logging.Logger:
+    def reset(self) -> None:
+        self.problem_occurred = False
+        self.logger.handlers.clear()
+        self._get_logger()
+
+    @classmethod
+    def _get_logger(cls, logger_name: str = "root") -> logging.Logger:
         """Helper function to setup logging. This is necessary, because the default logging does not work in the case of Parallel processing.
         https://github.com/joblib/joblib/issues/1017#issuecomment-711723073
 
@@ -51,7 +48,7 @@ class Logger(BaseModel):
             Logger (logging.Logger)
         """
         logger = logging.getLogger(logger_name)
-        logger.setLevel(LOG_LEVELS[log_level])
+        logger.setLevel(LOG_LEVELS[cls.log_level])
 
         if len(logger.handlers) == 0:
             stream_handler = logging.StreamHandler()
@@ -60,11 +57,19 @@ class Logger(BaseModel):
             )
             logger.addHandler(stream_handler)
 
-            save_path.parent.mkdir(parents=True, exist_ok=True)
-            file_handler = logging.FileHandler(save_path, mode="w")
+            cls._save_path().parent.mkdir(parents=True, exist_ok=True)
+            file_handler = logging.FileHandler(cls._save_path(), mode="w")
             file_handler.setFormatter(logging.Formatter("%(levelname)-8s %(message)s"))
             logger.addHandler(file_handler)
         return logger
+
+    @staticmethod
+    def _save_path() -> Path:
+        return (
+            Path(__file__).absolute().parents[2]
+            / "logs"
+            / (datetime.now().strftime("%Y.%m.%d_%H.00") + ".log")
+        )
 
     def critical(self, *args: Any) -> None:
         if self.log_level_is_on["CRITICAL"]:
